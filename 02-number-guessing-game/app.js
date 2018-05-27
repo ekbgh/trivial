7253d5c41e4a53dcaa2e9f52d2b1453ddf82e6b3
ekbgh <ebaidoo13@gmail.com>
2018-05-26 20:20:39 -0500

    Added Lose Case and Game Over

--- 02-number-guessing-game/app.js
+++ 02-number-guessing-game/app.js
@@ -1,7 +1,7 @@
 //Game Values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = Math.floor(Math.random() * max) + min,
   guessesLeft = 3;
 
 //UI Elements
@@ -12,3 +12,63 @@ const game = document.querySelector("#game"),
   guessInput = document.querySelector("#guess-input"),
   message = document.querySelector(".message");
 
+//Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//Play Agaon Event Listener
+game.addEventListener('mousedown', function(e) {
+  if (e.target.classList.contains('play-again')) {
+    window.location.reload();
+  }
+})
+//Listen for guesses
+guessBtn.addEventListener('click', function() {
+  let guess = parseInt(guessInput.value);
+
+  //Validate Input
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Enter a number between ${min}  and ${max}`,'red');
+    guessInput.style.borderColor = 'red';
+    return
+  }
+
+  if (guess === winningNum) {
+    //Game over
+    gameOver(true,`${guess} is correct, You WIN!`);
+  } else {
+      //Wrong number
+      guessesLeft--
+      if (!guessesLeft) {
+        //Game Over
+        gameOver(false,`You LOSE!, GAME OVER, Correct answer was ${winningNum}`);
+      } else {
+        //Game continues -- answer wrong
+        //Set Message
+        setMessage(`${guess} is incorrect, ${guessesLeft} guesses left`,'red');
+        //Clear input
+        guessInput.value = '';
+        //Change border color
+        guessInput.style.borderColor = 'red';
+        }
+    }
+})
+
+//Game Over
+function gameOver(won,msg) {
+  let color = won ? 'green':'red'
+  // Disable Input
+  guessInput.disabled = true;
+  //Change border color
+  guessInput.style.borderColor = color;
+  //Set message
+  setMessage(msg,color);
+  //Play Again?
+  guessBtn.value = 'Play Again?'
+  guessBtn.classList.add('play-again')
+}
+//set Message
+function setMessage(msg,color) {
+  message.textContent = msg;
+  message.style.color = color;
+}
